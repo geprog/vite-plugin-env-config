3f4aa702388d6e66d1c58636d0ec0e5c2bc17c17
Lukas <lukas@slucky.de>
2022-01-27 23:39:09 +0100

    fix: use globalThis instead of global (#9)
    * fix: use globalThis instead of global
    
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis
    
    * fix: replace all occurrences of global

--- src/getEnvConfig.ts
+++ src/getEnvConfig.ts
@@ -3,11 +3,11 @@ export interface EnvConfig {
 }
 
 export function getEnvConfig<T extends keyof EnvConfig>(key: keyof EnvConfig): EnvConfig[T] | undefined {
-  const _window = global.window as unknown as { env?: EnvConfig };
+  const _window = globalThis.window as unknown as { env?: EnvConfig };
   if (_window?.env) {
     return _window.env[key];
   }
-  if (global.process?.env) {
-    return global.process.env[key];
+  if (globalThis.process?.env) {
+    return globalThis.process.env[key];
   }
 }
