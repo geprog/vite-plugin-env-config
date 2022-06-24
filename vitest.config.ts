5ef1deb132434cd0a32626a24fbc877360192e6e
Lukas <lukas@slucky.de>
2022-06-24 18:24:05 +0200

    chore: use vitest (#23)
    * chore: use vitest
    
    * coverage action skip steps
    
    * remove coverage action
    
    * require full test coverage
    
    * update deps

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  test: {
+    coverage: {
+      '100': true,
+    },
+  },
+});
